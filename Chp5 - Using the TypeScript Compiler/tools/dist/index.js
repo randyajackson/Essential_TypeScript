af2a29a0c3358eb0f33ef80c5ca6f97416d5d3ff
randyajackson <rajackson.dev@gmail.com>
2019-12-03 14:00:36 -0700

    Chapter 5

---
+++ Chp5 - Using the TypeScript Compiler/tools/dist/index.js
@@ -0,0 +1,7 @@
+function printMessage(msg) {
+    console.log(`Message: ${msg}`);
+}
+printMessage("Hello, TypeScript");
+// //forcing an error
+// printMessage(100);
+// printMessage(700);
---
+++ Chp5 - Using the TypeScript Compiler/tools/src/index.ts
@@ -0,0 +1,8 @@
+function printMessage(msg: string): void {
+    console.log(`Message: ${ msg }`);
+}
+
+printMessage("Hello, TypeScript");
+// //forcing an error
+// printMessage(100);
+// printMessage(700);
