a21a95e5c42ed77e0639575147b1d9ba2085e436
randyajackson <rajackson.dev@gmail.com>
2019-12-16 16:53:14 -0700

    Chapter 8

--- Chp8 - Using Functions/types/dist/index.d.ts
+++ Chp8 - Using Functions/types/dist/index.d.ts
@@ -1,2 +1,4 @@
-declare function calculateTax(amount: any): number;
+declare function calculateTax(amount: any, discount?: any): number;
+declare function calculateTax2(amount: any, discount?: number, something?: number): number;
+declare function calculateTax3(amount: any, discount?: number, ...extraFees: number[]): number;
 declare let taxValue: number;
--- Chp8 - Using Functions/types/dist/index.js
+++ Chp8 - Using Functions/types/dist/index.js
@@ -1,5 +1,33 @@
-function calculateTax(amount) {
-    return amount * 1.2;
+// function calculateTax(amount) 
+// {
+//     return amount * 1.2;
+// }
+// function calculateTax(amount, discount) 
+// {
+//     return calculateTax(amount) - discount;
+// }
+//consolidating both above into one function
+function calculateTax(amount, discount) {
+    return (amount * 1.2) - (discount || 0);
 }
-let taxValue = calculateTax(100);
-console.log(`Total Amount: ${taxValue}`);
+function calculateTax2(amount, discount = 0, something = 0) {
+    return (amount * 1.2) - (discount);
+}
+function calculateTax3(amount, discount = 0, ...extraFees) {
+    return (amount * 1.2) - discount
+        + extraFees.reduce((total, val) => total + val, 0);
+}
+let taxValue = calculateTax(100, 0);
+console.log(`2 args: ${taxValue}`);
+taxValue = calculateTax(100);
+console.log(`1 arg: ${taxValue}`);
+// taxValue = calculateTax(100, 10, 20);
+// console.log(`3 args: ${taxValue}`);
+taxValue = calculateTax2(100, 0);
+console.log(`2 args: ${taxValue}`);
+taxValue = calculateTax2(100);
+console.log(`1 arg: ${taxValue}`);
+taxValue = calculateTax2(100, 10, 20);
+console.log(`3 args: ${taxValue}`);
+taxValue = calculateTax3(100, 10, 20, 1, 30, 7);
+console.log(`6 args: ${taxValue}`);
